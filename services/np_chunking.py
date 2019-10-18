e0b101966e0cc4cff53927f2bbb3fa03dbcc39a2
Pankaj <mepankaj@live.in>
2019-10-17 23:24:18 -0400

    .gitignore is now working

--- services/np_chunking.py
+++ services/np_chunking.py
@@ -1,53 +1,22 @@
-import nltk
-from nltk.corpus import stopwords
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
 
-text = "how many patients were 30 years old"
+def get_continuous_chunks(text):
+    chunked = ne_chunk(pos_tag(word_tokenize(text)))
+    continuous_chunk = []
+    current_chunk = []
+    for i in chunked:
+            if type(i) == Tree:
+                    current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+            elif current_chunk:
+                    named_entity = " ".join(current_chunk)
+                    if named_entity not in continuous_chunk:
+                            continuous_chunk.append(named_entity)
+                            current_chunk = []
+            else:
+                    continue
+    return continuous_chunk
 
-sentence_re = r'(?:(?:[A-Z])(?:.[A-Z])+.?)|(?:\w+(?:-\w+)*)|(?:\$?\d+(?:.\d+)?%?)|(?:...|)(?:[][.,;"\'?():-_`])'
-lemmatizer = nltk.WordNetLemmatizer()
-stemmer = nltk.stem.porter.PorterStemmer()
-grammar = r"""
-    NBAR:
-        {<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
-        
-    NP:
-        {<NBAR>}
-        {<NBAR><IN><NBAR>}  # Above, connected with in/of/etc...
-"""
-chunker = nltk.RegexpParser(grammar)
-toks = nltk.regexp_tokenize(text, sentence_re)
-postoks = nltk.tag.pos_tag(toks)
-print(postoks)
-tree = chunker.parse(postoks)
-stopwords = stopwords.words('english')
+my_sent = "WASHINGTON -- In the wake of a string of abuses by New York police officers in the 1990s, Loretta E. Lynch, the top federal prosecutor in Brooklyn, spoke forcefully about the pain of a broken trust that African-Americans felt and said the responsibility for repairing generations of miscommunication and mistrust fell to law enforcement."
 
-def leaves(tree):
-    """Finds NP (nounphrase) leaf nodes of a chunk tree."""
-    for subtree in tree.subtrees(filter = lambda t: t.label()=='NP'):
-        yield subtree.leaves()
-
-def normalise(word):
-    """Normalises words to lowercase and stems and lemmatizes it."""
-    word = word.lower()
-    # word = stemmer.stem_word(word) #if we consider stemmer then results comes with stemmed word, but in this case word will not match with comment
-    word = lemmatizer.lemmatize(word)
-    return word
-
-def acceptable_word(word):
-    """Checks conditions for acceptable word: length, stopword. We can increase the length if we want to consider large phrase"""
-    accepted = bool(2 <= len(word) <= 40
-        and word.lower() not in stopwords)
-    return accepted
-
-
-def get_terms(tree):
-    for leaf in leaves(tree):
-        term = [ normalise(w) for w,t in leaf if acceptable_word(w) ]
-        yield term
-
-terms = get_terms(tree)
-
-for term in terms:
-    for word in term:
-        print(word),
-    print
+get_continuous_chunks(my_sent)
